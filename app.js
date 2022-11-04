b493c75c7da92a99c01d06a369fc51b6eb13293b
jfmenard <jfmenard@hotmail.fr>
2022-11-04 12:58:57 +0100

    vercelback2

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var yearRouter = require('./routes/year');
 
 var app = express();
 const cors = require('cors');
@@ -16,6 +16,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/year', yearRouter);
 
 module.exports = app;

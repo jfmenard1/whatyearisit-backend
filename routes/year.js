23402a8095bdfbb8540f87a6f71f1d4108b8cf95
jfmenard <jfmenard@hotmail.fr>
2022-11-04 12:05:32 +0100

    versel1

---
+++ routes/year.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+
+  router.get('/', (req, res) => {
+    const date = new Date();
+    res.json({ year : date.getFullYear() + '' });
+   });
+
+
+module.exports = router;
